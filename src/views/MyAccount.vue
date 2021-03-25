a1783321fd77295359fd364ea6b0673add3177ab
Godbille Pascal <pascal.godbille@orange.fr>
2021-03-25 16:22:39 +0100

    requete findone marche

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,42 +1,38 @@
 <template>
   <div class="myAccount">
      <Nav></Nav>
-      <h1>Mon Compte</h1>
+      <h1>Mes informations</h1>
+      <div>{{this.$store.state.userStore.firstName}}</div>
+      <div>{{this.$store.state.userStore.lastName}}</div>
+      <div>{{this.$store.state.userStore.email}}</div>
+      <div>{{this.$store.state.userStore.nbPosts}}</div>
+      <div>{{this.$store.state.userStore.nbComments}}</div>
+      <div>{{this.$store.state.userStore.createdAt}}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+
 import Nav from '@/components/Nav.vue';
 import {mapState} from 'vuex';
+
 export default {
   
 	name: 'MyAccount',
+	data(){
+		return{
+  
+		};
+	},
 	components: {
 		Nav
 	},
 	computed:{
-		...mapState(['successSubscribe','token','isConnect','userId','isAdmin'])
+		...mapState(['successSubscribe','token','isConnect','userId','isAdmin','userStore'])
 	},
 	methods:{
-		async findOneUser () {
-			const requestOptions = {
-				method: "POST",
-				headers: { 
-					"Content-Type": "application/json",
-					"Authorization": `Bearer ${this.token}`},
-				body: JSON.stringify({
-					first_name:this.first_name,
-					last_name:this.last_name,
-					pseudo:this.pseudo,
-					email:this.email,
-					password:this.password
-				})
-			};
-			const response = await fetch(this.urlApi + `/users/${this.userId}`, requestOptions);
-			console.log(response);
-			this.dataResponse = await response.json();
-		}
+	
 	}
 };
 </script>

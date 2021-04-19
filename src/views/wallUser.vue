4b9335267591bef4c1d4d87b25b000d0f6d56394
Godbille Pascal <pascal.godbille@orange.fr>
2021-04-19 16:59:48 +0200

    debut walluser

--- src/views/wallUser.vue
+++ src/views/wallUser.vue
@@ -5,16 +5,17 @@
  <b-row class="wall">
 	<b-col md="3" sm>
 		<h4>{{user.first_name}} {{user.last_name}}</h4>
+		<h6> ({{user.pseudo}})</h6>
+		<div>Inscrit le {{momentDateMouse(user.createdAt)}}</div>
+		<!-- <div>Dernière connexion {{momentDate(user.updatedAt)}}</div> --><br>
 	</b-col>
-	<b-col  md="6" sm>
+	<b-col  md="7" sm style='padding:0 1.5 0 0 rem'>
 		<b-alert  :show="dismissCountDown" dismissible variant="success"  @dismissed="dismissCountDown=0"  @dismiss-count-down="countDownChanged">
 			Inscription reussie!</b-alert>
-		<b-link v-b-modal.publication @click='resetModal' style='text-decoration:none'>
+		<b-link v-b-modal.publication @click='resetModal' class="link">
+		<h2>Fil d'actualité</h2><br>
+		<h2 v-if='noPosts' style='font-style:italic; color:#FD2D01;'>{{noPosts}}</h2><br>
 		</b-link>
-		<h2>Fil d'actualité de {{allPostsByUserId[0].user.pseudo}}</h2>
-		<div>Inscrit le {{momentDateMouse(allPostsByUserId[0].user.createdAt)}}</div>
-	<div>Dernière connexion {{momentDateMouse(allPostsByUserId[0].user.updatedAt)}}</div><br>
-		<b-link  block variant="outline-secondary" class='postInput'  style='color:rgb(217, 120, 80 )' >{{textArea}}</b-link>
 <!-- modal publication -->
 		<b-modal id="publication" hide-footer size="lg" @close='alertCloseModal'>
 			<template #modal-title >
@@ -29,40 +30,96 @@
 				</div>
 			</div>
 			<b-button v-if='textArea !== ""' class="mt-5" variant='outline-primary' block @click="createPost">Publier</b-button>
+		</b-modal> 
+		<b-modal id="updatePublication" hide-footer size="lg" @close='alertCloseModal'>
+			<template #modal-title >
+			Modifier ma publication
+			</template>
+			<div class="d-block text-center">
+				<div>
+					<b-form-textarea  v-model='postToUpdate.post_content' autofocus id="updateTextarea" style='border: none;  -webkit-box-shadow: none;'   rows="1"	max-rows="10">
+
+					</b-form-textarea>
+					<pre class="mt-3 mb-0"></pre>
+				</div>
+			</div>
+			<b-button  class="mt-5" variant='outline-primary' block @click="updatePost">Modifier</b-button>
 		</b-modal>
 <!-- publications -->
-	<b-card title="" sub-title="" v-for="item in allPostsByUserId" :key="item.id" class="post">
-		<div class='headerCard'  >
-			<b-img   src="https://picsum.photos/50" fluid alt="Responsive image" class="authorImg"></b-img>
+	<b-card title="" sub-title="" v-for="(item,index) in allPosts" :key="item.id" class="post">
+		<div class='headerCard'>
+		<div  class='headerCard'>
+			<b-card-text ><b-img   src="https://picsum.photos/50" fluid alt="Responsive image" class="authorImg link"></b-img></b-card-text>
 				<div class='textHeader' >
-					<span   class="authorPost">{{item.user.pseudo}}</span>
-					<span v-b-tooltip.bottom.v-secondary ="momentDateMouse(item.createdAt)" >{{ momentDate(item.createdAt)}}</span>
-					</div>
+					<b-card-text  class="authorPost link">{{ item.user.pseudo}}</b-card-text>
+					<b-link class="link" v-b-tooltip.leftbottom.v-info ="momentDateMouse(item.createdAt)" >{{ momentDate(item.createdAt)}}</b-link>
+				</div>
 		</div>
-			<b-card-text class='textPost'>
-				{{item.post_content}}
-			</b-card-text>
+		<div class="menuHeader">...</div>
+		
+		</div>
+			<b-link v-if="item.user_id === $store.state.userId" class="link" v-b-modal.updatePublication @click='findOnePost(item.id)' ><b-card-text v-b-tooltip.right.hover.v-primary title="Modifier" class='textPost linkUser'>{{item.post_content}}</b-card-text></b-link>
+			<b-card-text v-else class='textPost '>{{item.post_content}}</b-card-text><br>
+			<div class="usersLikes" v-show='item.like.map(x=>x.user.pseudo).length>=1'><b-card-text v-b-tooltip.hover :title="item.like.map(x=>x.user.pseudo)" ><img src="https://res.cloudinary.com/dvtklgrcu/image/upload/v1618751389/Group_3rondjaime_fszx9r.svg" alt="" height="20" ><span v-html="item.like.map(x=>x.user.pseudo).length" style='margin-left:0.4rem;'></span></b-card-text></div>
+<!-- MenuPost -->	
 		<b-row class="likeComment" >
 			<b-col>
-				<b-button block variant="outline-secondary" class='btnLikeComment'><img src="https://res.cloudinary.com/dvtklgrcu/image/upload/v1616753162/comme_mwyvnb.svg" alt="" height="15">
-				<b-card-text class="like" >J'aime</b-card-text>
-				</b-button>
+				<div  v-on:click='function(){likePost(item.id,index); userLike(item);outFocusButton(index)}' block variant="outline-secondary" class='btnLikeComment'>
+					<div  v-if='userLike(item)'><img src="https://res.cloudinary.com/dvtklgrcu/image/upload/v1618753322/Group_1bluejaime_ymp6es.svg" alt="" height="22" ><b-card-text style='color:rgb(34,143,222); border:none'>J'aime</b-card-text></div>
+					<div  v-else><img   src="https://res.cloudinary.com/dvtklgrcu/image/upload/v1616753162/comme_mwyvnb.svg" alt="" height="15" style=' border:none'><b-card-text >J'aime</b-card-text></div>
+				</div>
+				
 			</b-col>
 			<b-col>
-				<b-button   block variant="outline-secondary" class='btnLikeComment'><img src="https://res.cloudinary.com/dvtklgrcu/image/upload/v1616754590/commentaire-bulle-ovale-blanche_vftrbh.svg" alt="" height="15">
-				<b-card-text class="comment"  >Commenter</b-card-text>
-				</b-button>
+				<div  @click="function(){setFocusInput( index ); switchToUpdate=false}" block variant="outline-secondary" class='btnLikeComment'>
+					<div>
+						<img src="https://res.cloudinary.com/dvtklgrcu/image/upload/v1616754590/commentaire-bulle-ovale-blanche_vftrbh.svg" alt="" height="15">
+						<b-card-text >Répondre</b-card-text>
+					</div>
+				</div>
 			</b-col>
-			<b-col v-if="item.user_id === $store.state.userId || $store.state.isAdmin">
-				<b-button  @click='deletePost(item.id)' block variant="outline-secondary"  class='btnLikeComment' ><img src="https://res.cloudinary.com/dvtklgrcu/image/upload/v1616755730/delete_sg8ndk.svg" alt="" height="15">
-				<b-card-text class="delete"   >Supprimer</b-card-text>
-			</b-button>
+			<b-col v-if="item.user_id === userId || user.is_admin === 1">
+				<div :data-key="index" @click='deletePost(item.id,index)' block variant="outline-secondary"  class='btnLikeComment' >
+					<div>
+						<img src="https://res.cloudinary.com/dvtklgrcu/image/upload/v1616755730/delete_sg8ndk.svg" alt="" height="15">
+						<b-card-text  >Supprimer </b-card-text>
+					</div>
+				</div>
 			</b-col>
 		</b-row> 
+
+<!-- comments -->
+<div class='commentGroup' block>
+	<div v-for="(comment) in allPosts[index].tblComments" :key="comment.id" class='commentAndAction'>
+		<b-card class="commentCard">
+				<div >
+					<b-link href="#" class="link" style='font-size:0.7rem'>{{comment.user.pseudo}}</b-link>
+					<b-card-text   class='textPost'><img src="" alt="">{{ comment.comment_content }}</b-card-text>
+				</div>
+		</b-card>
+<!-- menu Comments -->
+		<div v-if="comment.user_id === userId || user.is_admin === 1"  block variant="outline-secondary"  class='link actionsComment' style='font-size:0.6rem'>
+			<b-link class='link updateComment'  @click='findOneComment(comment.id,index)'>Modifier</b-link>
+			<b-link class='link deleteComment' @click='deleteComment(comment.id)' >Supprimer</b-link>
+		</div>
+	</div>
+</div>	
+<!-- input comment -->
+
+		<b-form-group  v-if='switchToUpdate !== false'>
+			<b-input   :data-key="index" class="inputComment"  v-model='commentToUpdate.comment_content' v-on:keyup.enter="updateComment(index)"></b-input>
+		</b-form-group>
+		<b-form-group  v-else>
+			<b-input   :data-key="index" class="inputComment"  v-model='newComment[index]' v-on:keyup.enter="createComment(item.id,user.id,index)"></b-input>
+		</b-form-group>
+
+
+		
+			
 	</b-card>
-	</b-col>
-	<b-col  md= '3' sm>
-	<div></div>
+</b-col>
+	<b-col  md= '2' sm> 
+	
 	</b-col>
 </b-row>	
 </div>
@@ -78,12 +135,21 @@ export default {
 	name: 'wall',
 	data(){
 		return{
-			textArea: "Quoi de neuf?" + this.$store.state.user.first_name + "......",
-			first_name:this.$store.state.user.first_name,
-			last_name:this.$store.state.user.last_name,
-			pseudo: this.$store.state.user.pseudo,
+			isUserLike:[],
+			noPosts:null,
+			switchToUpdate:false,
+			postToUpdate:{},
+			//commentToUpdate:'er',
+			newComment:[],
+			allComments:[],
+			startComment:-1, //false, //0,
+			dropdownDisplay :'display:none',
+			textArea:"Quoi de neuf? " + this.$store.state.user.pseudo + "......",
+			//first_name:this.$store.state.user.first_name,
+			//last_name:this.$store.state.user.last_name,
+			//pseudo: this.$store.state.user.pseudo,
 			password:null,
-			email:this.$store.state.user.email,
+			//email:this.$store.state.user.email,
 			dismissSecs: 5,
 			dismissCountDown: 0,
 			urlApi:'http://localhost:3000/api/groupomania',
@@ -93,6 +159,9 @@ export default {
 		Nav
 	},
 	computed:{
+		allPosts() {
+			return this.$store.state.allPosts;
+		},
 		allPostsByUserId() {
 			return this.$store.state.allPostsByUserId;
 		},
@@ -108,6 +177,46 @@ export default {
 			'allPostsByUserId']),
 	},
 	methods:{
+		userLike(item){
+			if(item.like.length !== 0) {
+				console.log('ok');
+				return item.like.includes(item.like.find(el=>el.user.id === this.user.id));
+				
+				
+					
+			}
+		
+		},
+		setFocusInput(index) {
+			const inputs = document.querySelectorAll('.inputComment');
+			inputs.forEach(ele => { 
+				if (ele.dataset.key == index) {
+					ele.focus();
+				} 
+			});
+		},
+		outFocusInput(index) {
+			const inputs = document.querySelectorAll('.inputComment');
+			console.log(inputs);
+			inputs.forEach(ele => { 
+				if (ele.dataset.key == index) {
+					ele.blur();
+				} 
+			});
+		},
+		outFocusButton(index) {
+			const buttons = document.querySelectorAll('.btnLikeComment');
+			console.log(buttons);
+			buttons.forEach(ele => { 
+				if (ele.dataset.key == index) {
+					ele.blur();
+				} 
+			});
+		},
+   
+		DisplayOn(){
+			return this.dropdownDisplay = this.dropdownDisplay ==='display:none'? 'display:block' : 'display:none';
+		},
 		momentDateMouse(item){
 			return moment(item).format('LLL');
 		
@@ -135,7 +244,7 @@ export default {
 		successSubscrirtionShow(){
 			this.$store.commit('SUCCESSSUBSCIBE');
 		},
-		allpostsInStore(allPostsData){
+		allPostsInStore(allPostsData){
 			this.$store.commit('ALLPOSTS',allPostsData);
 		},
 		allPostsByUserIdInStore(allPostsByUserIdData){
@@ -144,7 +253,7 @@ export default {
 		allUsersInStore(allUsersData){
 			this.$store.commit('ALLUSERS',allUsersData);
 		},
-		/* async findAllPosts() {
+		async findAllPosts() {
 			console.log('gege');
 			const requestOptions = {
 				method: "Get",
@@ -155,13 +264,17 @@ export default {
 			const response = await fetch(this.urlApi + `/posts`, requestOptions);
 			console.log(response);
 			this.allPostsData = await response.json();
-			console.log(this.$store.state.user);
 			if(this.allPostsData.count !== 0 && this.isConnect){
-				return this.allpostsInStore(this.allPostsData.data.rows);
-			} 
+				return this.allPostsInStore(this.allPostsData.data.rows);
+			} else if(this.allPostsData.count === 0){
+				this.noPosts = this.allPostsData.message;
+				return this.allPostsInStore("");
+			}else{
+				this.allPostsInStore('');
+			}
 		
-		}, */
-		async findAllPostsByUserId(userId) {
+		},
+		async findOnePost(postId) {
 			
 			const requestOptions = {
 				method: "Get",
@@ -169,14 +282,43 @@ export default {
 					"Content-Type": "application/json",
 					"Authorization": `Bearer ${this.token}`},
 			};
-			const response = await fetch(this.urlApi + `/posts/users/${userId}`, requestOptions);
-			console.log(response);
-			this.allPostsByUserIdData = await response.json();
-			console.log(this.$store.state.user);
-			if(this.allPostsByUserIdData.count !== 0 && this.isConnect){
-				return this.allPostsByUserIdInStore(this.allPostsByUserIdData.data.rows);
-			} 
+			const response = await fetch(this.urlApi + `/posts/${postId}`, requestOptions);
+			this.onePostData = await response.json();
+			this.postToUpdate = this.onePostData.data;
 		
+			
+		},
+		async findOneComment(commentId,index) {
+			
+			const requestOptions = {
+				method: "Get",
+				headers: { 
+					"Content-Type": "application/json",
+					"Authorization": `Bearer ${this.token}`},
+			};
+			const response = await fetch(this.urlApi + `/comments/${commentId}`, requestOptions);
+			this.oneCommentData = await response.json();
+			this.commentToUpdate = this.oneCommentData.data;
+			this.setFocusInput(index);
+			this.switchToUpdate = true;
+	
+		
+			
+		},
+		async findAllPostsByUserId(userId) {
+			const requestOptions = {
+				method: "Get",
+				headers: { 
+					"Content-Type": "application/json",
+					"Authorization": `Bearer ${this.token}`},
+			};
+			const response = await fetch(this.urlApi + `/posts/users/${userId}`, requestOptions);
+			this.allPostsByUserIdData = await response.json();
+			if(this.allPostsByUserIdData.count !== 0 && this.isConnect){
+				this.allPostsByUserIdInStore(this.allPostsByUserIdData.data.rows);
+			} else{
+				this.allPostsByUserIdInStore('');
+			}
 		},
 		async findAllUsers() {
 			const requestOptions = {
@@ -212,12 +354,58 @@ export default {
 			};
 			await fetch(this.urlApi + `/posts`, requestOptions);
 			await this.findAllPosts();
-			this.textArea= "Quoi de neuf?" + this.$store.state.user.first_name + "......";
+			this.textArea= "Quoi de neuf?" + this.$store.state.user.pseudo + "......";
+			this.noPosts=null;
 			
 			
 			
 		},
-		async deletePost(post){
+		async updatePost(){
+			this.$bvModal.hide('updatePublication');
+			console.log(this.postToUpdate);
+			const requestOptions = {
+				method: "Put",
+				headers: { 
+					"Content-Type": "application/json",
+					"Authorization": `Bearer ${this.token}`},
+				body:JSON.stringify({
+					post:{ 
+						post_content: this.postToUpdate.post_content,
+						userId: 2
+					}
+				})
+			};
+			await fetch(this.urlApi + `/posts/${this.postToUpdate.id}`, requestOptions);
+			await this.findAllPosts();
+			
+		
+			
+			
+		},
+		async updateComment(index){
+			this.$bvModal.hide('updatePublication');
+			console.log(this.commentToUpdate);
+			const requestOptions = {
+				method: "Put",
+				headers: { 
+					"Content-Type": "application/json",
+					"Authorization": `Bearer ${this.token}`},
+				body:JSON.stringify({
+					comment:{ 
+						comment_content: this.commentToUpdate.comment_content,
+						userId:this.commentToUpdate.user_id
+					}
+				})
+			};
+			await fetch(this.urlApi + `/comments/${this.commentToUpdate.id}`, requestOptions);
+			await this.findAllPosts();
+			this.commentToUpdate={};
+			this.switchToUpdate = false;
+			this.outFocusInput(index);
+			
+			
+		},
+		async deletePost(post,index){
 			//this.$bvModal.hide('publication');
 			//console.log(this.textArea);
 			const requestOptions = {
@@ -227,18 +415,105 @@ export default {
 					"Authorization": `Bearer ${this.token}`},
 				
 			};
-			await fetch(this.urlApi + `/posts/${post}`, requestOptions);
-			confirm('etes vous sur de vouloir supprimer ce post?');
+			const response =confirm('Etes vous sur de vouloir supprimer ce post?');
+			
+			
+			if(response){
+				await fetch(this.urlApi + `/posts/${post}`, requestOptions);
+				this.findAllPosts();
+				console.log(this.allPosts);
+			} else {
+				this.outFocusButton(index);
+				console.log('gege');
+			}
+			
+			
+			
+		},
+		async deleteComment(comment){
+			//this.$bvModal.hide('publication');
+			//console.log(this.textArea);
+			const requestOptions = {
+				method: "Delete",
+				headers: { 
+					"Content-Type": "application/json",
+					"Authorization": `Bearer ${this.token}`},
+				
+			};
+			await fetch(this.urlApi + `/comments/${comment}`, requestOptions);
+			
+			const response =confirm('Etes vous sur de vouloir supprimer ce commentaire?');
+			if(response){
+				this.findAllPosts();
+			} else {
+				console.log('gege');
+			}
+			
+			
+			
+		},
+		async createComment(postId, userId,index){
+			this.$bvModal.hide('publication');
+			//console.log(this.textArea);
+			
+			const requestOptions = {
+				method: "Post",
+				headers: { 
+					"Content-Type": "application/json",
+					"Authorization": `Bearer ${this.token}`},
+				body:JSON.stringify({
+					comment:{
+						comment_content: this.newComment[index],
+						post_id: postId,
+						user_id: userId
+					}
+				})
+			};
+			await fetch(this.urlApi + `/comments`, requestOptions);
+			await this.findAllPosts();
+			this.newComment=[];
+			this.outFocusInput(index);
+			/* if(response){
+				//this.findAllComments();
+				//this.findAllComments()
+			} else {
+				console.log('gege');
+			} */
+			
+			
+			
+		},
+		async likePost(postId){
+			console.log('gege');
+			const requestOptions = {
+				method: "Post",
+				headers: { 
+					"Content-Type": "application/json",
+					"Authorization": `Bearer ${this.token}`},
+				body:JSON.stringify({
+					like:{ 
+						postId: postId,
+						userId: this.userId
+					}
+				})
+			};
+			await fetch(this.urlApi + `/posts/like`, requestOptions);
 			this.findAllPosts();
 			
+		
 			
 			
-		}
+		},
 	},
 	mounted(){
+		console.log(this.allPosts);
+		this.findAllPosts();
+		this.findAllUsers();
 		
-		
-		
+		if (this.$store.state.successSubscribe){
+			this.showAlert();
+			this.successSubscrirtionShow();
+		}
 		
 	}
 };
@@ -246,11 +521,53 @@ export default {
 
 
 <style lang="scss" scoped>
+.usersLikes{
+	display:flex;
+	font-style:italic;
+	color:rgb(48, 104, 189)
+}
+.actionsComment{
+	align-self: flex-end;
+	
+}
+.deleteComment{
+	font-size: 0.7rem;
+	margin-right: 1.5rem;
+	margin-left: 0.5rem;
+}
+.updateComment{
+	font-size: 0.7rem;
+	
+}
+
+.commentCard{
+	display:flex;
+	width: fit-content;
+	text-align: left;
+	border-radius:30px;
+	margin-bottom:0.1rem;
+	margin-top:0.5rem;
+	background-color:rgb(235, 221, 221)
+	
+
+}
+.commentAndAction{
+	display:flex;
+	flex-direction: column;
+	width: fit-content;
+}
+
+.inputComment{
+	border-radius:30px;
+	margin: 0.8rem 0.5rem 0.5rem 0.5rem;
+	max-width:-webkit-fill-available;
+}
 .headerCard{
 	font-size: 0.8rem;
 	display:flex;
 	flex-direction: rows;
 	margin-bottom: 1rem;
+	justify-content:space-between ;
 
 }
 .textHeader{
@@ -258,7 +575,12 @@ export default {
 	flex-direction: column;
 	text-align: left;
 	margin-left:0.5rem;
+	
 }
+.menuHeader{
+	font-size:1rem
+}
+
 .authorImg{
 	border-radius: 50%;
 }
@@ -267,8 +589,9 @@ export default {
 	width:70%;
 }
 .card{
-	width:100%;
-	padding:1rem
+	//width:100%;
+	padding:0.6rem;
+
 }
 .wall{
 	margin-top: 5rem;
@@ -279,28 +602,40 @@ export default {
 
 .post {
 	margin-top:1rem;
-	
+	background-color:rgb(255, 252, 252)
+}
 .textPost{
 	text-align: justify;
 }	
 .btnLikeComment{
+	display:flex;
+	justify-content: center;
+	align-items: center;
+	height: 3rem;
 	padding:0;
-	border:none
+	border:none;	
+	text-decoration: none;
+	color: rgb(206, 48, 48);
+	font-size:0.8rem;
+	cursor: pointer;
+	border-top:1px rgb(235, 185, 185) solid;
+	border-bottom:1px rgb(235, 185, 185) solid
 }
-	
+.btnLikeComment:hover{
+	background-color: rgb(230, 210, 208);
+	cursor: pointer;
+	border-radius: 20rem;
+	color:rgb(34,143,222)
 }
 .likeComment {
 	display: flex;
 	flex-direction: row;
 	justify-content: space-around;
 	width:100%;
-	margin:0
+	margin:0;
+	margin-bottom: 0.5rem;
+}
 
-}
-.comment, .like, .delete {
-	text-decoration: none;
-	color: rgb(115, 20, 20);
-}
 .postInput{
 	overflow:hidden;
 	margin:0rem;
@@ -310,16 +645,22 @@ export default {
 	display: flex;
 	align-items: center;
 	text-decoration:none;
-	border:1px rgb(224, 201, 201) solid
+	border:1px rgb(224, 201, 201) solid;
+	padding-left:0.8rem;
 }
 .card-body{
 	padding:0;
 	
 	
+	
 }
-.link{
-	text-decoration: none;
+.link, .link:hover{
+	text-decoration:none;
 	color:black
 }
+.linkUser:hover{
+color:rgb(164, 61, 145)
+}
+////////////////////////////////@forward 
 
 </style>
